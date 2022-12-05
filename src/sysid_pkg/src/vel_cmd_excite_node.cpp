77454fae4ee0f9a2b646e5a5bbe88e679e7f3514
Jeremy Hopwood <jeremyhopwood@vt.edu>
2022-12-05 13:47:30 -0500

    updated velocity command node

--- src/sysid_pkg/src/vel_cmd_excite_node.cpp
+++ src/sysid_pkg/src/vel_cmd_excite_node.cpp
@@ -19,30 +19,27 @@
 #include <mavros_msgs/ManualControl.h>
 #include <mavros_msgs/ActuatorControl.h>
 #include <geometry_msgs/TwistStamped.h>
+#include <sensor_msgs/Imu.h>
+
+#include <Eigen/Dense>
 
 #include "sysid_tools.h"
 
 /**
  * @section Parameters to configure 
  */
-//
+
 // Debugging mode
-//
 #define DEBUG true
-//
+
 // CSV input file(s)
 // Note: Must use absolute filepath.
 // Note: first column of CSV file must be integers representing miliseconds.
 // Because we use a hashmap to access this data, the
 // intervals may be irregular, but must be ordered.
-//
 const string file0 = "/home/nsl/src/RotorSysID_ws/src/sysid_pkg/src/InputCSVs/ms_4axis_T30_f01-2_100hz.csv";
 const int T = 30;
 const int fs = 100;
-//
-// gain
-//
-const double K = 5.0;
 
 /**
  * @brief Fucntion for publishing debugging info to debug_pub
@@ -58,21 +55,22 @@ void Debug( ros::Publisher debug_pub, string info_str ) {
 /**
  * @section Callback functions that get data from the relevant MAVROS topic
  */
-//
+
 // State of pixhawk e.g. armed/disarmed, connected, etc.
 // ~state (mavros_msgs/State)
-//
 mavros_msgs::State current_state;
 void state_cb( const mavros_msgs::State::ConstPtr& msg ) {
 	current_state = *msg;
 }
-//
+
 // RC inputs (in raw milliseconds)
 // ~rc/in (mavros_msgs/RCIn)
-//
 mavros_msgs::RCIn rc_input;
 double amp = 0.0;
 int PTI_PWM = 0;
+unsigned int vel = 0;
+double mag = 0;
+double dir = 0;
 double da_cmd, de_cmd, dr_cmd, dt_cmd;
 void rcin_cb( const mavros_msgs::RCIn::ConstPtr& msg ) {
 	rc_input = *msg;
@@ -81,31 +79,40 @@ void rcin_cb( const mavros_msgs::RCIn::ConstPtr& msg ) {
 	dr_cmd = 2.0*(rc_input.channels[3]-1500)/796.0;
 	dt_cmd = 1.0*(rc_input.channels[2]-1102)/796.0;
 	PTI_PWM = rc_input.channels[6];
-	amp = 1.0*(rc_input.channels[9]-1102)/796.0; // R Knob, amp in (0, 1) - Use for excitation amplitude
+	if (rc_input.channels[7] > 1666) {
+		vel = 2;
+	} else if (rc_input.channels[7] > 1333) {
+		vel = 1;
+	} else {
+		vel = 0;
+	}
+	if (rc_input.channels[8] > 1666) {
+		mag = 10.0;
+	} else if (rc_input.channels[8] > 1333) {
+		mag = 5.0;
+	} else {
+		mag = 0.0;
+	}
+	if (rc_input.channels[9] > 1500) {
+		dir = 1.0;
+	} else {
+		dir = -1.0;
+	}
+	amp = 1.0*(rc_input.channels[10]-1102)/796.0; // R Knob, amp in (0, 1) - Use for excitation amplitude
 }
-//
+
 // RC Rx, interpreted and normalized.
 // ~manual_control/control (mavros_msgs/ManualControl)
-//
 mavros_msgs::ManualControl manual_input;
 void manual_cb( const mavros_msgs::ManualControl::ConstPtr& msg ) {
 	manual_input = *msg;
 }
-//
-// Velocity data from FCU.
-// ~local_position/velocity (geometry_msgs/TwistStamped)
-//
-geometry_msgs::TwistStamped body_vel_data;
-void body_vel_cb( const geometry_msgs::TwistStamped::ConstPtr& msg ) {
-	body_vel_data = *msg;
-}
-//
-// Velocity fused by FCU.
-// ~global_position/gp_vel (geometry_msgs/TwistStamped)
-//
-geometry_msgs::TwistStamped inert_vel_data;
-void inert_vel_cb( const geometry_msgs::TwistStamped::ConstPtr& msg ) {
-	inert_vel_data = *msg;
+
+// IMU data from FCU.
+// ~imu/data (sensor_msgs/Imu)
+sensor_msgs::Imu imu_data;
+void imu_cb( const sensor_msgs::Imu::ConstPtr& msg ) {
+	imu_data = *msg;
 }
 
 /**
@@ -115,20 +122,20 @@ int main( int argc, char **argv ) {
 	/**
 	 * @section Initialize variables and ROS node
 	 */
-	//
-	// Initialize the phase of the switches
-	//
+	// Initialize variables
 	bool PTI = false;
-	//
-	// initialize timestamps
-	//
 	double t0, t1;
-	//
+	geometry_msgs::Quaternion q0;
+	Eigen::Vector3d vb_ref;
+	Eigen::Vector3d vi_ref;
+	Eigen::Vector3d vb_ms;
+	Eigen::Vector3d vi_ms;
+	vector<float> ms(4);
+	vector<float> input(4);
+
 	// Load the CSV file into a map
-	//
 	std::map<int,vector<float>> InputData;
 	load_data(InputData, file0);
-	vector<float> input(4);
 	if (InputData.count(1)<1) {
 	    ROS_ERROR("Input data map not created!");
 	    return 0;
@@ -136,47 +143,38 @@ int main( int argc, char **argv ) {
 	#if DEBUG
 		ROS_INFO_STREAM("Input data map created successfully!");
 	#endif
-	//
+
 	// Initialize the node and create the node handle
-	//
 	ros::init(argc, argv, "vel_cmd_excite_node");
 	ros::NodeHandle nh;
-	//
+
 	// Create subscribers and publishers
-	//
 	ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
 		("mavros/state", 1, state_cb);
 	ros::Subscriber rc_in_sub = nh.subscribe<mavros_msgs::RCIn>
 		("mavros/rc/in", 1, rcin_cb);
 	ros::Subscriber manual_in_sub = nh.subscribe<mavros_msgs::ManualControl>
 		("mavros/manual_control/control", 1, manual_cb);
-	ros::Subscriber body_vel_sub = nh.subscribe<geometry_msgs::TwistStamped>
-		("mavros/local_position/velocity_body", 1, body_vel_cb);
-	ros::Subscriber inert_vel_sub = nh.subscribe<geometry_msgs::TwistStamped>
-		("mavros/local_posiiton/velocity_local", 1, inert_vel_cb);
+	ros::Subscriber imu_sub = nh.subscribe<sensor_msgs::Imu>
+		("mavros/imu/data", 1, imu_cb);
 	ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>
 		("mavros/setpoint_velocity/cmd_vel_unstamped", 1);
-	ros::Publisher actuator_control_pub = nh.advertise<mavros_msgs::ActuatorControl>
-		("mavros/actuator_control", 1);
 	ros::Publisher debug_pub = nh.advertise<std_msgs::String>
 		("debug", 10);
-	//
+
 	// check debugging topic
-	//
 	#if DEBUG
 		Debug(debug_pub, "Published to debug sucessfully!");
 	#endif
-	//
+
 	// ROS rate:
 	// 		Defines the rate at which the control loop runs.
 	//		Note that the code will not run faster than this rate,
 	//		but may run slower if its computationally taxing.
 	//		The setpoint publishing rate MUST be faster than 2Hz.
-	//
 	ros::Rate rate((double)fs);
-	//
+
 	// wait for FCU connection
-	//
 	while ( ros::ok() && !current_state.connected ) {
 		ROS_INFO_STREAM("Waiting!");
 		#if DEBUG
@@ -185,14 +183,14 @@ int main( int argc, char **argv ) {
 		ros::spinOnce();
 		rate.sleep();
 	}
-	//
-	// Creates the variable used to send velocity and actuator commands
-	//
+
+	// Create the variable used to send velocity commands
 	geometry_msgs::Twist cmd_vel;
-	mavros_msgs::ActuatorControl actuator_control;
-	//
+
+	// initialize other variables
+	
+
 	// send a few setpoints before starting
-	//
 	for (int i = 100; ros::ok() && i > 0; --i) {
 		ros::spinOnce();
 		rate.sleep();
@@ -206,70 +204,66 @@ int main( int argc, char **argv ) {
 	 * @details While everything is okay, loop at the specified rate, SAMPLERATE
 	 */
 	while (ros::ok()) {
-		/**
-		 * @section Check the PTI switch
-		 */
-		//
+
 		// If the PTI switch it OFF,
-		//
 		if (!PTI) {
-			//
-			// Pass through manual inputs as velocity commands with amp = m/s
-			//
-			cmd_vel.linear.x = K*amp*da_cmd;
-			cmd_vel.linear.y = K*amp*de_cmd;
-			cmd_vel.linear.z = amp*(2.0*dt_cmd-1.0);
-			cmd_vel.angular.z = amp*dr_cmd;
-			//
-		    // Publish
-		    //
-		    cmd_vel_pub.publish(cmd_vel);
-			//
+
+			// Get body velocity commands from rc inputs.
+			// However, keep the verical command in the inertial frame.
+			vb_ref << da_cmd, de_cmd, 0.0;
+
+			// Convert body velocity commands to the NED frame.
+			q0 = imu_data.orientation;
+			Eigen::Quaterniond q1(q0.w, q0.x, q0.y, q0.z);
+			Eigen::Matrix3d R_IB = q1.toRotationMatrix();
+			vi_ref = R_IB*vb_ref;
+
+			// Pass through manual inputs as velocity commands.
+			cmd_vel.linear.x = vi_ref(0);
+			cmd_vel.linear.y = vi_ref(1);
+			cmd_vel.linear.z = (2.0*dt_cmd-1.0);
+			cmd_vel.angular.z = 45*(3.1415926/180.0)*dr_cmd;
+
 			// If we have switched to PTI mode, capture initial conditions, etc.
-			//
 			if ( PTI_PWM > 1500 ) {
 				#if DEBUG
 					Debug(debug_pub, "PTI On");
 				#endif
-				//
+
 				// update PTI logical
-				// 
 				PTI =  true;
-				//
+	
 				// capture initial time
-				//
 				t0 = ros::Time::now().toSec();
 			}
-		//
+
 		// If the PTI switch is ON,
-		//
 		} else { 
-			//
-			// Read the current state of the aircraft if necessary...
-			//
-			//
-			//
-			// Pass exitation signal as velocity commands
-			//
+			
+			// Get body velocity commands from aucillary rc switches.
+			vb_ref << 0.0, 0.0, 0.0;
+			vb_ref(vel) = dir*mag;
+
+			// Convert body velocity commands to the NED frame.
+			q0 = imu_data.orientation;
+			Eigen::Quaterniond q1(q0.w, q0.x, q0.y, q0.z);
+			Eigen::Matrix3d R_IB = q1.toRotationMatrix();
+			vi_ref = R_IB*vb_ref;
+
+			// Also convert multisine commands to the NED frame.
 			t1 = ros::Time::now().toSec();
 			int time_idx = (int)(floor((t1-t0)*(double)fs)) % (T*fs); // time index in miliseconds
-			input = InputData[time_idx]; // get vector from map
-			#if DEBUG
-				Debug(debug_pub, "t_ms      = "+to_string(time_idx));
-				Debug(debug_pub, "multisine = "+to_string(input[0])+","+to_string(input[1])+","+to_string(input[2])+","+to_string(input[3]));
-			#endif
+			ms = InputData[time_idx]; // get vector from map
+			vb_ms << ms[0], ms[1], ms[2]; // get velocity components
+			vi_ms = R_IB*vb_ms; // transform to NED frame
 
-			cmd_vel.linear.x = K*amp*input[0]; // K*amp m/s
-			cmd_vel.linear.y = K*amp*input[1]; // K*amp m/s
-			cmd_vel.linear.z = 0.5*K*amp*input[2]; // 0.5*K*amp m/s
-			cmd_vel.angular.z = 6*K*(3.1415926/180.0)*amp*input[3]; // 6*K*amp deg/s
-			//
-		    // Publish
-		    //
-		    cmd_vel_pub.publish(cmd_vel);    
-			//
+			// Assemble velocity commands.
+			cmd_vel.linear.x = amp*vi_ms(0) + vi_ref(0);
+			cmd_vel.linear.y = amp*vi_ms(1) + vi_ref(1);
+			cmd_vel.linear.z = amp*vi_ms(2) + vi_ref(2);
+			cmd_vel.angular.z = 90*(3.1415926/180.0)*amp*ms[3];
+			
 			// If the PTI switch has been set back to off, set the PRI bool to false
-			//
 			if ( PTI_PWM <= 1500 ) {
 				#if DEBUG
 					Debug(debug_pub, "PTI Off");
@@ -278,19 +272,16 @@ int main( int argc, char **argv ) {
 			}
 		}
 		
-		/**
-		 * @section Publish velocity commands
-		 */
+		// Publish velocity commands
+		cmd_vel_pub.publish(cmd_vel);
 		#if DEBUG
-			Debug(debug_pub, "vel_cmd   = "+to_string(cmd_vel.linear.x)+","+to_string(cmd_vel.linear.y)+","+to_string(cmd_vel.linear.z)+","+to_string(cmd_vel.angular.z));
+			Debug(debug_pub, "u = "+to_string(cmd_vel.linear.x)+","+to_string(cmd_vel.linear.y)+","+to_string(cmd_vel.linear.z)+","+to_string(cmd_vel.angular.z));
 		#endif
-		//
+
 		// Calls any remaining callbacks
-		//
 		ros::spinOnce();
-		//
+
 		// Sleep for a period of time based on the ROS rate defined
-		//
 		rate.sleep();
 	}
 
