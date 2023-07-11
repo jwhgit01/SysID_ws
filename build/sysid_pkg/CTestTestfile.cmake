4027f5215da921ec3c6e4566fcd54fcc99564f9e
jwhgit01 <jeremyhopwood@vt.edu>
2023-07-11 08:56:21 -0400

    gitignore

--- build/sysid_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nsl/src/SysID_ws/src/sysid_pkg
-# Build directory: /home/nsl/src/SysID_ws/build/sysid_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/sysid_pkg/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/nsl/src/SysID_ws/src/sysid_pkg/lib".split(';') if "/home/nsl/src/SysID_ws/src/sysid_pkg/lib" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "sysid_pkg"
-PROJECT_SPACE_DIR = "/home/nsl/src/SysID_ws/devel"
-PROJECT_VERSION = "0.0.0"
